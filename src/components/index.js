61ad056cc3fee21d844c21dd70c86acbfd733310
dikigambol <westlakewetland@gmail.com>
2026-01-04 10:56:57 +0700

    refactor: simplify widget registry by removing default data and unused functions

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,3 @@
-// Widget registry - maps widget type/label to component
-import React from 'react';
-
 // Import komponen yang kita punya
 import ContainerWidget from "./ContainerWidget";
 import TextWidget from "./TextWidget";
@@ -9,31 +6,13 @@ export const widgetRegistry = {
     "Container Widget": {
         component: ContainerWidget,
         defaultSize: { w: 4, h: 4 },
-        defaultData: {
-            gridLayout: []
-        }
     },
     "Text Widget": {
         component: TextWidget,
         defaultSize: { w: 2, h: 3 },
-        defaultData: {
-            text: "Widget Teks"
-        }
     },
-};
-
-export const getWidgetConfig = (label) => {
-    return widgetRegistry[label] || null;
 };
 
 export const getWidgetComponent = (label) => {
     return widgetRegistry[label]?.component || null;
-};
-
-export const getWidgetDefaultSize = (label) => {
-    return widgetRegistry[label]?.defaultSize || { w: 4, h: 3 };
-};
-
-export const getWidgetDefaultData = (label) => {
-    return widgetRegistry[label]?.defaultData || {};
 };
